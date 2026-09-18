b623d6d526adcb5fc672fa7fd8c505397c90b6f2
Andre Sheiji Nacamura <a2321173@vmpdc.utfpr>
2026-09-18 15:40:02 -0300

    tratamento de erro das vagas

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-vagas = 150
+vagas = 0
 def consultarVagas():
     print(vagas)
 
 def estacionar():
     global vagas
-    vagas = vagas - 1
+    if(vagas == 0):
+        print("não há mais vagas")
+    else:
+        vagas = vagas - 1
 
 def liberarVaga():
     global vagas
+    
     vagas = vagas + 1
     
 
